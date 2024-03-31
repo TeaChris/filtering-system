975ac0d694a76c2229f9193ff5efc2e392e4ce57
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-03-31 12:57:03 +0100

    feat(page): home sort and db connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,33 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+import { ChevronDown, Filter } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+
+// define constants in UPPERCASE
+const SORT_OPTIONS = [
+  { name: 'None', value: 'none' },
+  { name: 'Price: Low to High', value: 'price-asc' },
+  { name: 'Price: High to Low', value: 'price-desc' },
+] as const
+
 export default function Home() {
+  const [filter, setFilter] = useState({
+    // color: ['beige', 'blue', 'green', 'purple', 'white'],
+    // price: { isCustom: false, range: DEFAULT_CUSTOM_PRICE },
+    size: ['L', 'M', 'S'],
+    sort: 'none',
+  })
+
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
@@ -8,8 +35,60 @@ export default function Home() {
           High-quality cotton selection
         </h1>
 
-        <div className="flex items-center"></div>
+        <div className="flex items-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+              Sort
+              <ChevronDown className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="end">
+              {SORT_OPTIONS.map((option) => (
+                <button
+                  key={option.name}
+                  className={cn('text-left w-full block px-4 py-2 text-sm', {
+                    'text-gray-900 bg-gray-100': option.value === filter.sort,
+                    'text-gray-500': option.value !== filter.sort,
+                  })}
+                  onClick={() => {
+                    setFilter((prev) => ({
+                      ...prev,
+                      sort: option.value,
+                    }))
+
+                    // _debouncedSubmit()
+                  }}
+                >
+                  {option.name}
+                </button>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <button className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden">
+            <Filter className="h-5 w-5" />
+          </button>
+        </div>
       </div>
+
+      <section className="pb-24 pt-6">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+          {/* Filters */}
+          <div className="hidden lg:block">
+            <ul className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
+              {/* {SUBCATEGORIES.map((category) => (
+                <li key={category.name}>
+                  <button
+                    disabled={!category.selected}
+                    className='disabled:cursor-not-allowed disabled:opacity-60'>
+                    {category.name}
+                  </button>
+                </li>
+              ))} */}
+            </ul>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
